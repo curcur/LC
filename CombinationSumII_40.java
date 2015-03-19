632bb44b3282fc89e7000f57138ef2a121f53f8e
Yuan Mei <curcur@users.noreply.github.com>
2015-03-19 00:38:34 -0700

    clean

--- CombinationSumII_40.java
+++ CombinationSumII_40.java
@@ -1,30 +1,60 @@
+Combination Sum II
+    - Given a collection of candidate numbers (C) and a target number (T), 
+      find all unique combinations in C where the candidate numbers sums to T.
+
+    - Each number in C may only be used once in the combination.
+
+Note:
+    - All numbers (including target) will be positive integers.
+    - Elements in a combination (a1, a2, … , ak) must be in 
+      non-descending order. (ie, a1 ≤ a2 ≤ … ≤ ak).
+    - The solution set must not contain duplicate combinations.
+
+    - For example, given candidate set 10,1,2,7,6,1,5 and target 8, 
+      A solution set is: 
+      [1, 7] 
+      [1, 2, 5] 
+      [2, 6] 
+      [1, 1, 6] 
+
+/**
+ * Related: 39 Combination Sum 
+ */
+
+/**
+ * Any number of combinations sum up to a target:
+ * - Requirements: a number can be chosen only once & no duplication
+ * - Having duplicates in input
+ * - Use DFS with pruning instead
+ */
 public class Solution {
-    List<List<Integer>> reslists = new ArrayList<>();
-    int[] cand;
+    List<List<Integer>> reslists = new ArrayList<>();  // the results
+    int[] cand;  // the sorted input number
     
     public List<List<Integer>> combinationSum2(int[] num, int target) {
         if (num.length == 0)  return reslists;
-        cand = num;   Arrays.sort(cand);
-        Stack<Integer> path = new Stack<>();
+        cand = num;   
+	Arrays.sort(cand);
+        
+	Stack<Integer> path = new Stack<>();
         combinationSum(0, path, target);
         return reslists;
     }
     
     private void combinationSum(int start, Stack<Integer> path, int target) {
-        
-        for(int i=start; i<cand.length;) {
+	for(int i=start; i<cand.length;) {
             if (target == cand[i]) {
                 List list = (List)(path.clone()); 
                 list.add(target);
                 reslists.add(list);
-                return;
+                return;  // cand[i] > target will be pruned
             } else if (target > cand[i]) {
                 path.push(cand[i]);
                 combinationSum(i+1, path, target - cand[i]);
                 //combinationSum(i, path, target - cand[i]);
                 path.pop(); i++;
 		while(i<cand.length && cand[i] == cand[i-1]) i++;    
-            } else return;
+            } else return;  // cand[i] > target will be pruned
         }
     }
 }
--- CombinationSum_39.java
+++ CombinationSum_39.java
@@ -1,106 +1,62 @@
-/**
- * 1) Sorting
- * 2) does not require sequential, hence no sliding window
- * 3) arbituary numbers, no hash or left-right pointers
- * 
- * -- It's like subsets 78, having constraints
- * list[i] = combinations ended with candidates[i] that smaller than target
- * 
- * -- can have arbitrary repeated numbers
- * -- can not be duplicated
- * ==>so repeat from beginning of intermediate list if not a repeated number
- *       repeat from previous of intermediate list if it is a repeated number
- * 
- * XXXXXXXXXXXXXXX   Time Limit Exceeded  XXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX
- * This is actually not equal to the subset problem since a number can be repeatedly used.
- */
- 
-/*public class Solution {
-    public List<List<Integer>> combinationSum(int[] candidates, int target) {
-        List<List<Integer>> reslists = new ArrayList<>();
-        if (candidates.length == 0) return reslists;
-        
-        Arrays.sort(candidates);
-        List<Comb> interlists = new ArrayList<>();
-        Comb comb = new Comb(0);   interlists.add(comb);
-        comb = new Comb(candidates[0]); comb.comblist.add(candidates[0]); interlists.add(comb);
-        int prev = 1;
-        
-        for(int i=1; i<candidates.length; i++) {
-            if (candidates[i] > target || candidates[i] + candidates[0] > target) break;
-            int j = (candidates[i] == candidates[i-1]) ? prev : 0;
-            int length = interlists.size(); prev = length;
-            for(; j<length; j++) {
-                comb = new Comb(interlists.get(j));
-                if (comb.sum + candidates[i] <= target) comb.append(candidates[i]);
-                if (comb.sum == target) reslists.add(comb.comblist);
-                else if (comb.sum < target) interlists.add(comb);
-            }
-        }
-        return reslists;
-    }
+Combination Sum
+    - Given a set of candidate numbers (C) and a target number (T), 
+      find all unique combinations in C where the candidate numbers sums to T.
+
+    - The same number may be repeatedly chosen from C unlimited number of times.
+
+Note:
+    - All numbers (including target) will be positive integers.
+    - Elements in a combination (a1, a2, … , ak) must be in 
+      non-descending order. (ie, a1 ≤ a2 ≤ … ≤ ak).
+    - The solution set must not contain duplicate combinations.
     
-    private class Comb {
-        List<Integer> comblist;
-        int sum;
-        
-        Comb(Comb comb) {
-            this.comblist = new ArrayList<>(comb.comblist);
-            this.sum = comb.sum;
-        }
-        
-        Comb(int sum) {
-            this.comblist = new ArrayList<>();
-            this.sum = sum;
-        }
-        
-        void append(int n) {
-            this.comblist.add(n);
-            sum += n;
-        }
-    }
-}*/
+    - For example, given candidate set 2,3,6,7 and target 7, 
+      A solution set is: [7] [2, 2, 3] 
 
 /**
- * The above is kind of a level first traversal
- * this may work when getting all subsets, but needs to store everything,
- * in the case where n is large, but do not need to find all subsets, DFS might be better
+ * Related: 40 Combination Sum II
+ *          78 Subsets
  */
- 
+
 /**
- * two ways of DFS
- * 1st way DFS: the first number has candidates 2,3,6,7, second 3,6,7...
- * 2nd way DFS: -- 2 can be chosen or not, -- 3 can be chosen or not.... (XXXXX not possible because a number can be chosen unlimited times)
+ * Any number of combinations sum up to a target:
+ * - Requirements: a number can be chosen any times & no duplication
+ * - BFS (like 78 subsets) needs to store many intermediate results
+ * - Use DFS with pruning instead
+ * - Steps: sorting - easy for pruning
+ *          the first number has candidates, 2, 3, 6, 7
+ *          the second number has candidates >= the first number... 2, 3, 6, 7
  */
+
 public class Solution {
-    List<List<Integer>> reslists = new ArrayList<>();
-    int[] cand;
+    List<List<Integer>> reslists = new ArrayList<>();  // the results
+    int[] cand;  // the sorted input number
     
     public List<List<Integer>> combinationSum(int[] candidates, int target) {
         if (candidates.length == 0)  return reslists;
-        cand = candidates;   Arrays.sort(cand);
-        Stack<Integer> path = new Stack<>();
+        cand = candidates;   
+	Arrays.sort(cand);
+        
+	Stack<Integer> path = new Stack<>();
         combinationSum(0, path, target);
         return reslists;
     }
     
     private void combinationSum(int start, Stack<Integer> path, int target) {
-        
-        for(int i=start; i<cand.length;) {
+        for(int i=start; i<cand.length; i++) {
             if (target == cand[i]) {
                 List list = (List)(path.clone()); 
                 list.add(target);
                 reslists.add(list);
-                return;
+                return;   // cand[i] > target will be pruned
             } else if (target > cand[i]) {
                 path.push(cand[i]);
                 //combinationSum(i+1, path, target - cand[i]);
                 combinationSum(i, path, target - cand[i]);
-                path.pop(); i++;
-                // I thought they have duplicates in input
+                path.pop(); 
+                // I thought input has duplicates, use this in II
                 // while(i<cand.length && cand[i] == cand[i-1]) i++;    
-                //  XXXXXXX use this in follow up
-            } else return;
+	    } else return; // cand[i] > target will be pruned
         }
     }
 }
