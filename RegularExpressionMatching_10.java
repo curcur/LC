22ce5d0d0a51ac2de4f8eed4934a9812aa2911f2
Yuan Mei <curcur@users.noreply.github.com>
2015-03-19 15:11:00 -0700

    no comment

---
+++ RegularExpressionMatching_10.java
@@ -0,0 +1,42 @@
+/**
+ * This kind of problem can always be solved by Recursion.
+ * - Corner case: s is empty, p is X*
+ * 
+ * - We start by solving '*'
+ * - X* can be think of as 0, 1, 2,..., as many X as possible
+ *      - match(s, i, p, j+2)  "0 X"
+ *      - match(s, i+1, p, j)   if s[i] matches p[j]
+ * - if there is no * following X, then p[i] has to match with s[j] 
+ * - DP method next time
+ */
+
+public class Solution {
+    public boolean isMatch(String s, String p) {
+        return isMatch(s, 0, p, 0);
+    }
+    
+    // s[j] vs p[i]
+    private boolean isMatch(String s, int j, String p, int i) {
+        
+        // when pattern reaches the end, string should also reach the end
+        if (i == p.length())   return j == s.length();
+    
+        if (i+1 < p.length() && p.charAt(i+1) == '*') {
+            if (isMatch(s, j, p, i+2))  return true;  //  0 X
+            
+            if (j < s.length() && 
+		(s.charAt(j) == p.charAt(i) || p.charAt(i) == '.'))
+                return isMatch(s, j+1, p, i);
+            
+            // The above statement is equivalent to the following statement
+            // while(j < s.length() && 
+	    //	  (s.charAt(j) == p.charAt(i) || p.charAt(i) == '.'))
+            //    if (isMatch(s, ++j, p, i+2))    return true;
+        }
+        else if (j < s.length() 
+		 && (s.charAt(j) == p.charAt(i) || p.charAt(i) == '.'))
+            return isMatch(s, j+1, p, i+1);
+        
+        return false;
+    }
+}
