c97857d93baf5a23e85745d31cc1e38012962245
Yuan Mei <curcur@users.noreply.github.com>
2015-05-06 01:36:42 -0700

    second pass

--- PalindromePartitioning_131.java
+++ PalindromePartitioning_131.java
@@ -1,9 +1,43 @@
 /**
- * 1. Recursion
- * - Since needs to retuan all possible combinations, so DFS might be good
- * - for each s(0...i) is palindrome, 
- *   combine with all possible partitions(s(i+1...length-1))
+ * ----------------------------------------------------------------------------
+   Palindrome Partitioning
+    - Given a string s, partition s such that every substring of the partition 
+      is a palindrome.
+    - Return all possible palindrome partitioning of s.
+
+   For example, given s = "aab",
+    - Return
+    [
+      ["aa","b"],
+      ["a","a","b"]
+    ]
+ * ----------------------------------------------------------------------------
+ */
+
+/**
+ * Related: 132 Palindrome Partitioning II
+ *          5   Longest Palindromic Substring
+ * Tags: Palindrome, Substring, Recursion, Dynamic Programming, Partition
+ */
+
+/**
+ * Keywords: Partition, All lists
+ * - This problem needs to return all possible results, hence the dynamic 
+ *   programming (BFS) needs to store many intermediate results.
+ * - 132 only needs to optimize the mincut, so is much more suitable for
+ *   dynamic programming.
+ * - Recursion may be more suitable for this problem.
+ *
  * - DFS does not waste spaces, however, waste computation
+ * - BFS does not waste computation, however, waste spaces
+ *   
+ * 1. Recursion
+ * - Since we need to retuan all possible combinations, so DFS might be good
+ * - for each substring s(0...i) which is a palindrome, 
+ *   combine with all possible partitions(s(i+1...length-1))
+ * - Time Complexity is exponential because
+ *   - T(n) = n^2 + T(n-1) + T(n-2) + ...T(1)
+ *   - Roughly O(n^2*2^n)
  */
 
 public class Solution {
@@ -12,7 +46,7 @@ public class Solution {
         int length = s.length();
         if (length == 0)
             return res;
-               
+	
         for(int i=1; i<=length; i++) {
             String subleft = s.substring(0, i);
             if(isPalindrome(subleft)) {
@@ -43,15 +77,18 @@ public class Solution {
     }
 }
 
+
+//------------------------------------------------------------------------------
+
 /**
- * 2. Without Recursion, Similar to 131. Palindrome Partitioning II
+ * 2. DP without Recursion, Similar to 131. Palindrome Partitioning II
  * - res[i]: all possible partition lists till s(i)
- *      Hence, res[i] = res[j] + if s(j,i) is palin     
- *      for each j == i-1, ..., 0
- * 
+ *   - Hence, res[i] = res[j] + if s(j,i) is palin     
+ *     for each j == i-1, ..., 0
  * - How to check whether s(j,i) is palin? O(n)
- *      - we can use s(j+1, i-1) is palin && s(j) == s(i) ?
- *      - this suggests another DP (two dimensional)
+ *   - we can use s(j+1, i-1) is palin && s(j) == s(i) ?
+ *   - this suggests another DP (two dimensional)
+ * - O(n^2) time, space is exponential
  */
 
 public class Solution {
