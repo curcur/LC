9b71e484bb3d4eae708f51fb8e35b6f2d53d7ec4
Yuan Mei <curcur@users.noreply.github.com>
2015-05-07 01:32:49 -0700

    second pass

--- RotateArray_189.java
+++ RotateArray_189.java
@@ -1,28 +1,58 @@
 /**
- * 1. move right one by one
- * 2. initialize another array, copy the ori[length-k, length-1] to dest[0, k-1]; ori[0, length-k-1] to dest[k, length-1]
- * 3. rotate twice
+ * ----------------------------------------------------------------------------
+   Rotate Array
+    - Rotate an array of n elements to the right by k steps.
+
+   For example
+    - with n = 7 and k = 3, 
+      the array [1,2,3,4,5,6,7] is rotated to [5,6,7,1,2,3,4].
+
+   Note:
+    - Try to come up as many solutions as you can, 
+    - there are at least 3 different ways to solve this problem.
+
+   Hint:
+    - Could you do it in-place with O(1) extra space?
+ * ----------------------------------------------------------------------------
+ */
+
+/**
+ * Related: 186 Reverse Words in a String II
+ *          61  Rotate List
+ * Tags: Palindrome, Integer
+ */
+
+/**
+ * - Method 1
+ *   - Shift the array to right one position each time, 
+ *   - O(nk) time, and O(1) space
+ *
+ * - Method 2
+ *   - Initialize another array length = k
+ *   - Copy the ori[length-k, length-1] to dest[0, k-1]; 
+ *   - Copy ori[0, length-k-1] to ori[k, length-1]
+ *   - Copy dest[0, k-1] back to ori[0, k-1]
+ *   - O(n+k) time, and O(k) space
+ *
+ * - Method 3 Reverse twice
+ * - what if k >= length ?
  */
 
 public class Solution {
     public void rotate(int[] nums, int k) {
-        if (nums == null)   return;
+	int length = nums.length;
+	k %= length;            // XXXX what if k>n?
         
-        k %= nums.length;           // XXXXXXX what if k>n?
-        
-        int left = 0, right = nums.length-1;
-        pRotate(nums, left, right);
-        
-        left = 0; right = k-1;      // XXXXXX k-1
-        pRotate(nums, left, right);
-        
-        left = k; right = nums.length-1;
-        pRotate(nums, left, right);
+        reverse(nums, 0, length-1);
+        reverse(nums, 0, k-1);  // XXXXXX k-1
+        reverse(nums, k, length-1);
     }
     
-    private void pRotate(int[] nums, int left, int right) {
+    private void reverse(int[] nums, int left, int right) {
         while(left < right) {
-            int temp = nums[left]; nums[left] = nums[right]; nums[right] = temp;
+            int temp = nums[left]; 
+	    nums[left] = nums[right]; 
+	    nums[right] = temp;
             left++; right--;
         }
         
