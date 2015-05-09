c06c573258520e1d2d1bbd8483367ac1f7b3df49
Yuan Mei <curcur@users.noreply.github.com>
2015-05-08 18:56:33 -0700

    second pass

--- PathSumII_113.java
+++ PathSumII_113.java
@@ -1,4 +1,34 @@
 /**
+ * ----------------------------------------------------------------------------
+   Path Sum II
+    - Given a binary tree and a sum, find all root-to-leaf paths where 
+      each path's sum equals the given sum.
+
+   For example:
+    - Given the below binary tree and sum = 22,
+
+              5
+             / \
+            4   8
+           /   / \
+          11  13  4
+         /  \    / \
+        7    2  5   1
+    
+    - Return
+      [
+        [5,4,11,2],
+        [5,8,4,5]
+      ]
+ * ----------------------------------------------------------------------------
+ */
+
+/**
+ * Related: 112 Path Sum
+ * Tags: Tree Path, DFS
+ */
+
+/**
  * Definition for binary tree
  * public class TreeNode {
  *     int val;
@@ -9,70 +39,37 @@
  */
  
 /**
- * This version uses up to much spaces!
+ * - Record the path from the root to the current node, and remaining sum
+ * - If reaching a leaf node and the total sum == target
+ *   add the path to the result lists
+ * - This kind of eturn all list problem, typically use a dfs (recursively)
+ * - recover the path stack to the original after finishing
+ *   one push & one pop
+ *   
+ * - all lists problem - return type is List<List<Somthing>>
  */
-/*public class Solution {
-    
-    List<List<Integer>> paths = new ArrayList<>();
-    public List<List<Integer>> pathSum(TreeNode root, int sum) {
-        List<Integer> path = new ArrayList<>();
-        
-        pathSum(root, sum, path);
-        return paths;
-    }
-    
-    private void pathSum(TreeNode root, int sum, List<Integer> path) {
-        if (root == null)   return;
-        path.add(root.val);
-        
-        if (root.left == null && root.right == null) {
-            if(sum == root.val) { paths.add(path); }
-            return;
-        }
-        
-        if (root.left != null) {
-            List<Integer> pathleft = new ArrayList<>(path);
-            pathSum(root.left, sum-root.val, pathleft);
-        }
-        if (root.right != null) {
-            List<Integer> pathright = new ArrayList<>(path);
-            pathSum(root.right, sum-root.val, pathright);
-        }
-        
-    }
-}*/
 
-
-/**
- * Try to only use one path list
- * pop_up the node every time you return
- */
 public class Solution {
-    
-    List<List<Integer>> paths = new ArrayList<>();
+    List<List<Integer>> res = new ArrayList<>();
     public List<List<Integer>> pathSum(TreeNode root, int sum) {
         Stack<Integer> path = new Stack<>();
-        
         pathSum(root, sum, path);
-        return paths;
+        return res;
     }
     
     private void pathSum(TreeNode root, int sum, Stack<Integer> path) {
         if (root == null)   return;
+        if (root.left == null && root.right == null)
+            if (root.val == sum) {
+                path.push(root.val);
+                res.add(new ArrayList<>(path));
+                path.pop();
+                return;
+            }
+        
         path.push(root.val);
-        
-        if (root.left == null && root.right == null) {
-            if(sum == root.val) { paths.add((List)path.clone()); }
-            path.pop();
-            return;
-        }
-        
-        if (root.left != null) {
-            pathSum(root.left, sum-root.val, path);
-        }
-        if (root.right != null) {
-            pathSum(root.right, sum-root.val, path);
-        }
+        pathSum(root.left, sum-root.val, path);
+        pathSum(root.right, sum-root.val, path);
         path.pop();
     }
 }
