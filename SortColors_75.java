502195065bd1a6ee263f18a16ef2892c41d1a074
Yuan Mei <curcur@users.noreply.github.com>
2015-05-18 21:51:21 -0700

    second pass

--- SortColors_75.java
+++ SortColors_75.java
@@ -1,10 +1,32 @@
 /**
- * The most naive way is O(n^2) or sorting better nlog(n), but in fact, 
- * We can do it in O(n) time
+ * ----------------------------------------------------------------------------
+   Sort Colors
+    - Given an array with n objects colored red, white or blue, 
+      sort them so that objects of the same color are adjacent, 
+      with the colors in the order red, white and blue.
+
+    - Here, we will use the integers 0, 1, and 2 to represent the 
+      color red, white, and blue respectively.
+
+   Note:
+    - You are not suppose to use the library's sort function for this problem.
+
+   Follow up:
+    - A rather straight forward solution is a two-pass algorithm using 
+      counting sort.
+    - First, iterate the array counting number of 0's, 1's, and 2's, 
+      then overwrite array with total number of 0's, then 1's and followed 
+      by 2's.
+
+    - Could you come up with an one-pass algorithm using only constant space?
+ * ----------------------------------------------------------------------------
  */
 
 /**
- * One pass, swap 0 to the left, and 2 to te right
+ * - The most naive way is sorting O(nlogn) or counting O(n) but two passes
+ * - this is just three thing sorting, so the key point is to find the 
+ *   two boundaries, l & r: 
+ *    - all 0s before l, all 2s aftre r, all 1s between l & r
  */
 
 public class Solution {
@@ -12,13 +34,16 @@ public class Solution {
         int l = 0, r = A.length-1, index = l;
         
         while(index <= r) {
-            if (A[index] == 0) { swap(A, l, index); l++; }
-            else if (A[index] == 1) { index++; }
-            else if (A[index] == 2) { swap(A, index, r); r--; }
-            else { System.err.println("Input Error"); }
-            
-            index = Math.max(l, index);
-        }
+	    if (A[index] == 2) { 
+		swap(A, index, r); 
+		r--; 
+	    } else if (A[index] == 1) { 
+		index++; 
+	    } else {
+		swap(A, l, index); 
+		l++; index++; 
+	    }
+	}
     }
     
     private void swap(int[] A, int i, int j) {
