71a7e05f26129f1c452785fe0cda33262de87ec3
Yuan Mei <curcur@users.noreply.github.com>
2015-05-18 22:35:06 -0700

    second pass

--- BestTime2BuySellStockII_122.java
+++ BestTime2BuySellStockII_122.java
@@ -1,6 +1,22 @@
 /**
- * buy low, sell high. Catch every of this opportunity
+ * ----------------------------------------------------------------------------
+   Best Time to Buy and Sell Stock II
+    - Say you have an array for which the ith element is the price of a given 
+      stock on day i.
+    - Design an algorithm to find the maximum profit. 
+    - You may complete as many transactions as you like 
+      (ie, buy one and sell one share of the stock multiple times). 
+
+    - However, you may not engage in multiple transactions at the same time 
+      (ie, you must sell the stock before you buy again).
+ * ----------------------------------------------------------------------------
  */
+
+/**
+ * - buy at every floor and sell at every ceiling
+ * - This is equivalent to catch every pair of buy low, sell high.
+ */
+
 public class Solution {
     public int maxProfit(int[] prices) {
         int profit = 0;
--- BestTime2BuySellStock_121.java
+++ BestTime2BuySellStock_121.java
@@ -1,9 +1,20 @@
 /**
- * XXXXX This is not that simple, the min should happen smaller than max
+ * ----------------------------------------------------------------------------
+   Best Time to Buy and Sell Stock
+    - Say you have an array for which the ith element is the price of a given 
+      stock on day i.
+     
+    - If you were only permitted to complete at most one transaction 
+    - (ie, buy one and sell one share of the stock), design an algorithm to 
+      find the maximum profit.
+ * ----------------------------------------------------------------------------
+ */
+
+/**
+ * XXXXX the min should happen smaller than max
  * 
- * DP -- O(n)
- * P(i): the maximal profit on ith day if buy and sell before or on day i; buy before sell 
- * P(i) == max { P(i-1), p - min }, update min if p < min
+ * - P[i]: the max profit we can get if selling on day_i
+ *       sell[i] - minprice till day_i
  */ 
  
 public class Solution {
@@ -12,8 +23,8 @@ public class Solution {
         
         int maxP = 0, minBuy = prices[0];
         for(int i=1; i<prices.length; i++) {
+	    minBuy = Math.min(minBuy, prices[i]);
             maxP = Math.max(maxP, prices[i]-minBuy);
-            minBuy = Math.min(minBuy, prices[i]);
         }
         return maxP;
     }
